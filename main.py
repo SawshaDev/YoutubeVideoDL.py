103a985d5cec921ec6c781a1167be6dd8a256b4d
callimarieYT <85613286+callimarieYT@users.noreply.github.com>
2021-11-15 22:31:24 -0800

    allows to switch between mp3 and mp4
    -now allows you to chose between mp3 and mp4
    -checks if file exist if you try to download same file twice

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from pytube import YouTube
-
+import os
 URL = input("Input url here: ")  
 video = YouTube(URL)
-video_streams = video.streams.filter(file_extension='mp4').get_by_itag(22)
-print(video_streams.title)
-name = video_streams.title
+video_streams1 = video.streams.filter(file_extension='mp4').get_by_itag(22)
+video_streams2 = video.streams.filter(only_audio=True).get_by_itag(139)
+print(video_streams1.title)
+name = video_streams1.title
+ftype = input("MP4 or MP3: ").lower()
 
-video_streams.download(filename = f"{name}.mp4", 
-	output_path = "video") 
+if ftype == "mp3":
+    video_streams2.download(filename = f"{name}.mp3", 
+    output_path = "video") 
+    
+    
+if ftype == "mp4":
+    video_streams1.download(filename=f"{name}.mp4",
+    output_path = "video")
+    
+
+if os.path.exists(f"video/{name}.{ftype}"):
+    print(f"The file {name}.{ftype} Already Exists!")
